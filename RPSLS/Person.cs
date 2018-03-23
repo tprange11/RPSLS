548f7ebb2fa3ab84322b7fd1db088558893fe3ef
tprange11 <tprange@gmail.com>
2018-03-23 10:47:53 -0500

    Created VS project and added Game, Person and Player classes

---
+++ RPSLS/Person.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RPSLS
+{
+    class Person
+    {
+        string name;
+
+        public Person(string name)
+        {
+            this.name = name;
+        }
+
+        public void DisplayName()
+        {
+            Console.WriteLine("Welcome " + name);
+        }
+    }
+}
